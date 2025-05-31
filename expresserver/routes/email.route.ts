136bd83038e4ba5ef7efe7a9ebea9adeab678888
saurabhhh777 <jeeaspirantwallah2022@gmail.com>
2025-06-01 00:59:27 +0530

    getEmail and sendEmail added :

--- expresserver/routes/email.route.ts
+++ expresserver/routes/email.route.ts
@@ -2,15 +2,19 @@ import express, { Request, RequestHandler, Response } from "express";
 
 const router = express.Router();
 
-import { sendEmail,getMail } from "../controllers/email.controller";
+import { sendEmail,getMail,addDomain } from "../controllers/email.controller";
+import {userAuth} from "../middlewares/userAuth.middleware";
 
 
-router.route("/sendEmail").post(sendEmail as RequestHandler);
 
-router.route("/getMail").get(getMail as RequestHandler);
+//saving the email to the database
+router.route("/addDomain").post(userAuth, addDomain );
 
+router.route("/sendEmail").post(userAuth ,sendEmail);
 
+router.route("/getMail").get(getMail);
 
 
 
 export default router;
+// This file defines the routes for email-related operations in the Express server.
